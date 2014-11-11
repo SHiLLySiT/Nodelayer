3bd1d6a3d8e0809557f499f885f4684f8429bcaa
Alex Larioza <alexlarioza@gmail.com>
2014-11-10 22:49:16 -0800

    updated application info

--- bat/SetupApplication.bat
+++ bat/SetupApplication.bat
@@ -8,7 +8,7 @@
 
 :: Your certificate information
 set CERT_NAME="Nodelayer"
-set CERT_PASS=fd
+set CERT_PASS=pixelsquidgames
 set CERT_FILE="bat\Nodelayer.p12"
 set SIGNING_OPTIONS=-storetype pkcs12 -keystore %CERT_FILE% -storepass %CERT_PASS%
 
