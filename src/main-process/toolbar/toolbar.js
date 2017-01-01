3621659d53958f685e8904bfb55ab14f40f67905
Alex Larioza <alexlarioza@gmail.com>
2017-01-01 16:31:36 -0600

    fixed toolbar not remembering selected tool when reopened

--- src/main-process/toolbar/toolbar.js
+++ src/main-process/toolbar/toolbar.js
@@ -4,7 +4,14 @@ const electron = require('electron');
 const ipc = electron.ipcMain;
 const remote = require('electron').remote
 
+global.currentTool = 'create';
+
 //---------------------------------------------------------------------- EVENTS
+ipc.on('request-tool', function(event) {
+    event.returnValue = global.currentTool;
+});
+
 ipc.on('tool-changed', function(event, tool) {
+    global.currentTool = tool;
     global.window.canvas.webContents.send('tool-changed', tool);
 });
